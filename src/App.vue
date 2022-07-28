3ac59223bae58f51f5eed41afce09c1e4fd95b6f
ksungsu <zxzxzlxlzlxl@naver.com>
2022-07-28 13:15:56 +0900

    spring boot + vue 게시판

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,12 @@
 // 컴포넌트 import
 import RegistBoard from "@/components/RegistBoard";
 import BoardList from "@/components/BoardList";
-import RegistForm from "@/components/RegistForm";
 
 export default {
   name: 'App',
   //변경한 component 이름 추가
   components: {
     BoardList,
-    RegistForm,
     RegistBoard,
   },
 };
--- src/components/RegistBoard.vue
+++ src/components/RegistBoard.vue
@@ -3,19 +3,23 @@
 <!--    글쓰기 버튼-->
     <el-button type="danger" @click="openPopup = true">글쓰기</el-button>
     <el-dialog title = "글쓰기" :visible.sync="openPopup" width="30%" center>
+<!--      v-model : Dto 객체 명 -->
+      <el-select v-model="category">
+        <el-option value="Java">Java</el-option>
+        <el-option value="Javascript">Javascript</el-option>
+        <el-option value="Database">Database</el-option>
+      </el-select>
       <el-input placeholder="작성자명을 입력해 주세요" v-model="writer"></el-input>
+      <el-input v-model="password"></el-input>
       <el-input placeholder="제목을 입력해 주세요" v-model="title"></el-input>
       <el-input
           style="margin-top: 30px"
         placeholder="내용을 입력해주세요"
         type="textarea"
-        :rows="30"
-        v-model="content">
+        v-model="text">
       </el-input>
-
-<!--        dialog-footer 영역-->
       <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="registBoard()">확인</el-button>
+        <el-button type="primary" @click="registBoard()" >확인</el-button>
         <el-button @click="openPopup=false">취소</el-button>
       </span>
     </el-dialog>
@@ -24,25 +28,38 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+
   name: "RegistBoard",
+
+  //$data와 같은 기능
   data(){
     return {
       openPopup:false,
       title:'',
-      content:'',
+      text:'',
+      writer: '',
+      category:'',
+      password:'',
     };
   },
+
+  //입력한 parameter 값
   computed:{
     setParams(){
       const params = {
         title : this.title,
-        content: this.content,
-        writer: this.writer
+        content: this.text,
+        writer: this.writer,
+        category: this.category,
+        password: this.password,
       };
       return params;
     },
   },
+
   methods:{
     registBoard(){
       axios
